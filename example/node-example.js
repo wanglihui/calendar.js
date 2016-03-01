e83be03f7f3dc5780be6a54778298cbdd9f1cfd5
wanglihui <lihui.wang@tulingdao.com>
2016-03-01 10:49:45 +0800

    修改例子错误

--- example/node-example.js
+++ example/node-example.js
@@ -2,7 +2,12 @@
  * Created by wlh on 16/2/26.
  */
 
-var Calendar = require("../lib/index");
+var Calendar = require("../lib/calendar");
 
-var cal = new Calendar();
-console.info(cal.renderMonth(2, 2016, 2));
+var cal = new Calendar({
+    show: function(data) {
+        console.info(data);
+    }
+});
+
+cal.renderMonth(2, 2016, 2);
