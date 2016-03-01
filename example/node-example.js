2c3db490ae7693539be04f686067e5d09f7fce85
wanglihui <lihui.wang@tulingdao.com>
2016-03-01 09:44:35 +0800

    update dir

--- example/node-example.js
+++ example/node-example.js
@@ -2,7 +2,7 @@
  * Created by wlh on 16/2/26.
  */
 
-var Calendar = require("./index");
+var Calendar = require("../lib/index");
 
 var cal = new Calendar();
 console.info(cal.renderMonth(2, 2016, 2));
